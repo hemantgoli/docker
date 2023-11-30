38275178bfeadb650aefddd8719d17b83b641ffc
“Hemanth <hemanthkumargoli@gmail.com>
2023-11-30 22:09:16 +0800

    updated docker file

---
+++ hello.py
@@ -0,0 +1,26 @@
+# multiple print statements based on quotos
+print("hello  there")
+print('hello world')
+print('''
+      hello
+            there
+                  how are you
+      ''')
+print("hello" + " " + "world")
+
+# require multi vars to get the user data
+name=input("what's your first name ?\n")
+print("welcome" + " "+ name)
+
+# one line to take input and print data
+print("welcome" + " " + input("what's your second name?\n"))
+
+# to find the lenght of the string
+print(len(input("what's your last name?\n")))
+
+
+
+
+
+
+
